dffa25d412dcbb3f926f4dfa8cdd09c5d0149718
RENoMafex <Mafexxx@aol.com>
2024-10-05 00:47:06 +0200

    automated commit

--- .customprompt.zsh
+++ .customprompt.zsh
@@ -2,6 +2,10 @@ function prompt_my_uptime() {
 	local uptime_seconds=$(cat /proc/uptime | awk '{print int($1)}')
 	local uptime_hours=$((uptime_seconds / 3600))
 	local uptime_minutes=$(( (uptime_seconds % 3600) / 60 ))
-
-	p10k segment -f green -t "uptime: $uptime_hours:$uptime_minutes"
+	
+	if (( uptime_minutes < 10 )); then
+		p10k segment -f green -t "uptime: $uptime_hours:0$uptime_minutes"
+	else
+		p10k segment -f green -t "uptime: $uptime_hours:$uptime_minutes"
+	fi
 }
