1d40a70eb05f7e559120ac24a736c9328c76036b
RENoMafex <Mafexxx@aol.com>
2025-01-27 20:21:31 +0100

    made even more debuggable

--- WIPmakefile.sh
+++ WIPmakefile.sh
@@ -1,35 +1,52 @@
 #!/bin/bash
+	set -x
+	echo ""
 	echo "Updating and upgrading the system via apt"
 	echo "Press any key to continue"
 	read -n 1 -s
+	echo ""
 	sudo apt update && sudo apt upgrade -y
+	echo ""
 	echo "Installing zsh"
 	echo "Press any key to continue"
 	read -n 1 -s
+	echo ""
 	sudo apt install -y zsh # Requiement for oh my zsh
+	echo ""
 	echo "Installing git"
 	echo "Press any key to continue"
 	read -n 1 -s
+	echo ""
 	sudo apt install -y git # Requiement for oh my zsh
+	echo ""
 	echo "Changing shell to zsh"
 	echo "Press any key to continue"
 	read -n 1 -s
+	echo ""
 	sudo chsh -s $(which zsh)
+	echo ""
 	echo "making temporary directory"
 	echo "Press any key to continue"
 	read -n 1 -s
+	echo ""
 	mkdir tempordir && cd tempordir
+	echo ""
 	echo "Installing oh my zsh"
 	echo "Press any key to continue"
 	read -n 1 -s
+	echo ""
 	sh -c "$(wget -O- https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+	echo ""
 	echo "Installing powerlevel10k"
 	echo "Press any key to continue"
 	read -n 1 -s
+	echo ""
 	git clone --depth=1 https://github.com/romkatv/powerlevel10k.git "${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k"
+	echo ""
 	echo "Downloading dotfiles"
 	echo "Press any key to continue"
 	read -n 1 -s
+	echo ""
 	git clone --depth=1 https://github.com/RENoMafex/dotfiles.git
 	cd dotfiles
 	cp -fv .p10k.zsh ~/.p10k.zsh
