a539a018c8690e6b22251fda1ffe24eb96ee5853
RENoMafex <Mafexxx@aol.com>
2025-01-27 19:43:17 +0100

    fixed typo

--- WIPmakefile.sh
+++ WIPmakefile.sh
@@ -1,35 +1,35 @@
 #!/bin/sh
 	echo "Updating and upgrading the system via apt"
 	echo "Press any key to continue"
-	read -n1 -s
+	read -n 1 -s
 	sudo apt update && sudo apt upgrade -y
 	echo "Installing zsh"
 	echo "Press any key to continue"
-	read -n1 -s
+	read -n 1 -s
 	sudo apt install -y zsh # Requiement for oh my zsh
 	echo "Installing git"
 	echo "Press any key to continue"
-	read -n1 -s
+	read -n 1 -s
 	sudo apt install -y git # Requiement for oh my zsh
 	echo "Changing shell to zsh"
 	echo "Press any key to continue"
-	read -n1 -s
+	read -n 1 -s
 	sudo chsh -s $(which zsh)
 	echo "making temporary directory"
 	echo "Press any key to continue"
-	read -n1 -s
+	read -n 1 -s
 	mkdir tempordir && cd tempordir
 	echo "Installing oh my zsh"
 	echo "Press any key to continue"
-	read -n1 -s
+	read -n 1 -s
 	sh -c "$(wget -O- https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 	echo "Installing powerlevel10k"
 	echo "Press any key to continue"
-	read -n1 -s
+	read -n 1 -s
 	git clone --depth=1 https://github.com/romkatv/powerlevel10k.git "${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k"
 	echo "Downloading dotfiles"
 	echo "Press any key to continue"
-	read -n1 -s
+	read -n 1 -s
 	git clone --depth=1 https://github.com/RENoMafex/dotfiles.git
 	cd dotfiles
 	cp -fv .p10k.zsh ~/.p10k.zsh
