207fc0e64923dd48a46ccd42b37d1e053995c717
RENoMafex <Mafexxx@aol.com>
2025-01-27 17:11:42 +0100

    renamed WIPmakefile

---
+++ WIPmakefile.sh
@@ -0,0 +1,38 @@
+#!/bin/sh
+	##      ####      ##    ##    ######
+	##      ####      ##    ##    #######
+	 ##    ##  ##    ##     ##    ##   ###
+	 ##    ##  ##    ##     ##    ##   ##
+	  ##  ##    ##  ##      ##    #######
+	  ##  ##    ##  ##      ##    #####
+	   ####      ####       ##    ##
+	   ####      ####       ##    ##
+	    ##        ##        ##    ##
+
+	sudo apt update
+	sudo apt install -y zsh # Requiement for oh my zsh
+	sudo apt install -y git # Requiement for oh my zsh
+	sudo chsh -s $(which zsh)
+	mkdir tempordir
+	cd tempordir
+	sh -c "$(wget -O- https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+	git clone --depth=1 https://github.com/romkatv/powerlevel10k.git "${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k"
+	git clone --depth=1 https://github.com/RENoMafex/dotfiles.git
+	cd dotfiles
+	cp -f .p10k.zsh ~/.p10k.zsh
+	cp -f .zshrc ~/.zshrc
+	cp -f .aliases ~/.aliases
+	cp -f .customprompt ~/.customprompt
+	cd ..
+	cd ..
+	rm -rf tempordir
+	
+	##      ####      ##    ##    ######
+	##      ####      ##    ##    #######
+	 ##    ##  ##    ##     ##    ##   ###
+	 ##    ##  ##    ##     ##    ##   ##
+	  ##  ##    ##  ##      ##    #######
+	  ##  ##    ##  ##      ##    #####
+	   ####      ####       ##    ##
+	   ####      ####       ##    ##
+	    ##        ##        ##    ##
