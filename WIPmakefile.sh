9a2a329b57415a62916ef15c44bd367347706257
RENoMafex <Mafexxx@aol.com>
2025-01-28 02:47:46 +0100

    removed debugging stuff

--- WIPmakefile.sh
+++ WIPmakefile.sh
@@ -1,69 +1,20 @@
 #!/bin/bash
-	echo ""
-	echo "Updating and upgrading the system via apt"
-	echo "Press any key to continue"
-	read -n 1 -s
-	echo ""
 	sudo apt update #&& sudo apt upgrade -y
-	echo ""
-	echo "Installing zsh"
-	echo "Press any key to continue"
-	read -n 1 -s
-	echo ""
 	sudo apt install -y zsh # Requiement for oh my zsh
-	echo ""
-	echo "Installing git"
-	echo "Press any key to continue"
-	read -n 1 -s
-	echo ""
 	sudo apt install -y git # Requiement for oh my zsh
-	echo ""
-	echo "Changing shell to zsh"
-	echo "Press any key to continue"
-	read -n 1 -s
-	echo ""
-	sudo -k chsh -s $(which zsh) $USER
-	echo ""
-	echo "making temporary directory"
-	echo "Press any key to continue"
-	read -n 1 -s
-	echo ""
+	sudo -k chsh -s "$(which zsh)" "$USER" # Change default shell to zsh
 	mkdir -v tempordir
 	cd tempordir
-	echo ""
-	echo "Downloading dotfiles"
-	echo "Press any key to continue"
-	read -n 1 -s
-	echo ""
 	git clone --depth=1 https://github.com/RENoMafex/dotfiles.git
 	cp -fv dotfiles/.p10k.zsh $HOME/.p10k.zsh
 	cp -fv dotfiles/.zshrc $HOME/.zshrc
 	cp -fv dotfiles/.aliases $HOME/.aliases
 	cp -fv dotfiles/.customprompt.zsh $HOME/.customprompt
-	echo ""
-	echo "Installing oh my zsh"
-	echo "Press any key to continue"
-	read -n 1 -s
-	echo ""
 	sh -c "$(wget -O- https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended --keep-zshrc
-	echo ""
-	echo "Installing powerlevel10k"
-	echo "Press any key to continue"
-	read -n 1 -s
-	echo ""
 	git clone --depth=1 https://github.com/romkatv/powerlevel10k.git "${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k"
-	echo ""
-	echo "Removing temporary directory"
-	echo "Press any key to continue"
-	read -n 1 -s
-	echo ""
 	cd ..
 	rm -rfv tempordir
-	echo ""
-	echo "Done"
-	echo "Press any key to continue"
-	read -n 1 -s
-	echo ""
+
 
 # To use this script, copy and paste the following command in your terminal
 # wget -O /tmp/WIPmakefile.sh https://raw.githubusercontent.com/RENoMafex/dotfiles/master/WIPmakefile.sh && bash /tmp/WIPmakefile.sh
